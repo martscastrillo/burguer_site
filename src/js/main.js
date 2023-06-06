e6c32cb45a0e50b81c511317c65aa78f45ab180d
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-06 13:05:00 +0200

    add signs

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ const js_menu = document.querySelector('.js_menu');
 const js_bars = document.querySelector('.js_bars');
 const js_column = document.querySelector('.js_column');
 const js_cross = document.querySelector('.js_cross');
-
+const js_menu_element1 = document.querySelector('.js_menu_element1');
+const js_menu_element2 = document.querySelector('.js_menu_element2');
+const js_menu_element3 = document.querySelector('.js_menu_element3');
 
 const open = () => {
     js_column.style.transition= 'all ease-out .5s';
@@ -13,7 +15,6 @@ const open = () => {
 const close = () => {
     js_column.style.transition= 'all ease-out .5s';
     js_column.style.transform= 'translateX(-100%)';
-
 }
 const hoverin = () => js_cross.style.color='rgb(238, 108, 238)';
 const hoverout = ()  => js_cross.style.color='white'
@@ -24,3 +25,6 @@ js_bars.addEventListener("click", open);
 js_cross.addEventListener("click", close);
 js_cross.addEventListener("mouseover", hoverin);
 js_cross.addEventListener("mouseout", hoverout);
+js_menu_element1.addEventListener("click", close);
+js_menu_element2.addEventListener("click", close);
+js_menu_element3.addEventListener("click", close);
