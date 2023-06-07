27f429eedd2a4fbf06f98c51e29c489bef28e9f7
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-07 21:09:37 +0200

    add details

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const js_menu = document.querySelector('.js_menu');
+
 const js_bars = document.querySelector('.js_bars');
 const js_column = document.querySelector('.js_column');
 const js_cross = document.querySelector('.js_cross');
@@ -12,19 +12,23 @@ const js_arrow = document.querySelector('.js_arrow');
 const open = () => {
     js_column.style.transition= 'all ease-out .5s';
     js_column.style.transform= 'translateX(0)';
+ 
 }
 const close = () => {
     js_column.style.transition= 'all ease-out .5s';
     js_column.style.transform= 'translateX(-100%)';
 }
-const hoverin = () => js_cross.style.color='rgb(238, 108, 238)';
+const hoverin = () => {
+    js_cross.style.color='rgb(238, 108, 238)';
+    js_cross.style.cursor= 'pointer';
+}
 const hoverout = ()  => js_cross.style.color='white'
 
 const apperarArrow = () => {
     js_arrow.classList.remove('collapsed')
 }
 
-js_menu.addEventListener("click", open);
+
 js_bars.addEventListener("click", open);
 js_cross.addEventListener("click", close);
 js_cross.addEventListener("mouseover", hoverin);
