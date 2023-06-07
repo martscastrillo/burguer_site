9e8d039b94d0828ca9c8382ca3c7d22f4e00c0fe
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-07 22:45:50 +0200

    add laste modification

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,30 @@
-'use strict';
+"use strict";
 
-
-const js_bars = document.querySelector('.js_bars');
-const js_column = document.querySelector('.js_column');
-const js_cross = document.querySelector('.js_cross');
-const js_menu_element1 = document.querySelector('.js_menu_element1');
-const js_menu_element2 = document.querySelector('.js_menu_element2');
-const js_menu_element3 = document.querySelector('.js_menu_element3');
-const js_arrow = document.querySelector('.js_arrow');
+const js_bars = document.querySelector(".js_bars");
+const js_column = document.querySelector(".js_column");
+const js_cross = document.querySelector(".js_cross");
+const js_menu_element1 = document.querySelector(".js_menu_element1");
+const js_menu_element2 = document.querySelector(".js_menu_element2");
+const js_menu_element3 = document.querySelector(".js_menu_element3");
+const js_arrow = document.querySelector(".js_arrow");
 
 const open = () => {
-    js_column.style.transition= 'all ease-out .5s';
-    js_column.style.transform= 'translateX(0)';
- 
-}
+	js_column.style.transition = "all ease-out .5s";
+	js_column.style.transform = "translateX(0)";
+};
 const close = () => {
-    js_column.style.transition= 'all ease-out .5s';
-    js_column.style.transform= 'translateX(-100%)';
-}
+	js_column.style.transition = "all ease-out .5s";
+	js_column.style.transform = "translateX(-100%)";
+};
 const hoverin = () => {
-    js_cross.style.color='rgb(238, 108, 238)';
-    js_cross.style.cursor= 'pointer';
-}
-const hoverout = ()  => js_cross.style.color='white'
+	js_cross.style.color = "rgb(238, 108, 238)";
+	js_cross.style.cursor = "pointer";
+};
+const hoverout = () => (js_cross.style.color = "white");
 
 const apperarArrow = () => {
-    js_arrow.classList.remove('collapsed')
-}
-
+	js_arrow.classList.remove("collapsed");
+};
 
 js_bars.addEventListener("click", open);
 js_cross.addEventListener("click", close);
@@ -37,4 +34,4 @@ js_menu_element1.addEventListener("click", close);
 js_menu_element2.addEventListener("click", close);
 js_menu_element3.addEventListener("click", close);
 
-window.addEventListener('scroll', apperarArrow);
+window.addEventListener("scroll", apperarArrow);
