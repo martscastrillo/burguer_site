8e6b22e85229170a5cb0c2ef160efe8f784bf058
Marta Castrillo <mart.castrillo@gmail.com>
2023-06-07 17:06:19 +0200

    Update docs

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ const js_cross = document.querySelector('.js_cross');
 const js_menu_element1 = document.querySelector('.js_menu_element1');
 const js_menu_element2 = document.querySelector('.js_menu_element2');
 const js_menu_element3 = document.querySelector('.js_menu_element3');
+const js_arrow = document.querySelector('.js_arrow');
 
 const open = () => {
     js_column.style.transition= 'all ease-out .5s';
@@ -19,6 +20,10 @@ const close = () => {
 const hoverin = () => js_cross.style.color='rgb(238, 108, 238)';
 const hoverout = ()  => js_cross.style.color='white'
 
+const apperarArrow = () => {
+    js_arrow.classList.remove('collapsed')
+}
+
 js_menu.addEventListener("click", open);
 js_bars.addEventListener("click", open);
 js_cross.addEventListener("click", close);
@@ -27,3 +32,5 @@ js_cross.addEventListener("mouseout", hoverout);
 js_menu_element1.addEventListener("click", close);
 js_menu_element2.addEventListener("click", close);
 js_menu_element3.addEventListener("click", close);
+
+window.addEventListener('scroll', apperarArrow);
